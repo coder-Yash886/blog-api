17786eeb0ba7544865a4af84f4f0e301d7630923
yash kumar <yashkumar.967565@gmail.com>
2026-01-30 10:24:51 +0530

    Added the features update post delete post

--- blog-api/controllers/postController.js
+++ blog-api/controllers/postController.js
@@ -60,3 +60,78 @@ const createPost = async function(req,res){
         })
      }
 }
+
+const updatePost = async function(req,res){
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({
+                success: false,
+                message: "post not found"
+            })
+        }
+
+        if(post.author.toString() !== req.user.id){
+            return res.status(403).json({
+                success: false,
+                message: "Your not update the post"
+            })
+        }
+
+        post = await Post.findByIdAndUpdate(req.params.id,req.body,{
+            new: true,
+            runValidators: true
+        })
+        res.status(200).json({
+            success: true,
+            message: "post updated successfully",
+            data: post
+        })
+    } catch(error){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+const deletePost = async function(req,res){
+    try{
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({
+                success: false,
+                message: "post not found"
+            })
+        }
+
+        if(post.author.toString() !== req.user.id){
+            return res.status(403).json({
+                success: false,
+                message: "You not delete the post"
+            })
+        }
+
+        await post.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            message: "post deleted successfully"
+        })
+    } catch(error){
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+module.exports = {
+    getPosts,
+    getPost,
+    createPost,
+    updatePost,
+    deletePost
+}
