2aceaf9f51a0cb825044842d5265045767908069
yash kumar <yashkumar.967565@gmail.com>
2026-01-30 09:37:11 +0530

    Post Schema

---
+++ blog-api/models/Post.js
@@ -0,0 +1,34 @@
+
+const mongoose = require("mongoose");
+const postSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 200
+    },
+    content:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    category: {
+        type: String,
+        default: "General"
+    },
+    tags:[{
+        type: String
+    }],
+    status:{
+        type: String,
+        enum: ['draft','published'],
+        default: 'draft'
+    }
+    },{ timestamps: true
+});
+
+module.exports = mongoose.model('Post',postSchema)
